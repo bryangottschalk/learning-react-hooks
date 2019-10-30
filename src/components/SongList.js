0f6f8b1834e5a27cb8a21736af0b26ab26504b88
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-10-30 09:15:06 -0500

    add 2 useState hooks --> 1 to map over songs and another to add new songs via form

---
+++ src/components/SongList.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import NewSongForm from './NewSongForm';
+
+const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'almost home', id: 1 },
+    { title: 'memory gospel', id: 2 },
+    { title: 'this wild darkness', id: 3 },
+  ]);
+  const addSong = title => {
+    setSongs([...songs, { title: title, id: songs.length + 1 }]);
+  };
+  return (
+    <div className="song-list">
+      <ul>
+        {songs.map(song => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
+        <li>this wild darkness</li>
+        <li>memory gospel</li>
+      </ul>
+      <NewSongForm addSong={addSong} />
+    </div>
+  );
+};
+
+export default SongList;
